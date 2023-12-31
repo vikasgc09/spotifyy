1e31f45588c5630105c72fe95e13e92f5c5d8e3b
Your Name <vikasgc09@gmail.com>
2023-12-31 13:37:19 +0530

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopNavComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'spotifyy';
+  title = 'Spotify - Web Player';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { SearchComponent } from './components/search/search.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SongcardComponent } from './components/songcard/songcard.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'button', component: ButtonComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'playlist', component: PlaylistComponent },
+    { path: 'search', component: SearchComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'song-card', component: SongcardComponent },
+    { path: 'top-nav', component: TopNavComponent },
+
+  ];
