48c9f2e5cc784bae47c29010b8c2e207ab41aa57
Vikas <Vikasgc09@gmail.com>
2024-01-01 18:11:57 +0530

    asdasd

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TopNavComponent } from '../top-nav/top-nav.component';
 import { SongcardComponent } from '../songcard/songcard.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopNavComponent, SongcardComponent],
+  imports: [TopNavComponent, SongcardComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
