c79f38bcb3bd449733e487a89d478870310ecd61
vikas Chauhan <vikasgc09@gmail.com>
2024-01-03 17:37:25 +0530

    Initial Commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TopNavComponent } from '../top-nav/top-nav.component';
 import { SongcardComponent } from '../songcard/songcard.component';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopNavComponent, SongcardComponent, RouterLink],
+  imports: [TopNavComponent, SongcardComponent, RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/components/songcard/songcard.component.ts
+++ src/app/components/songcard/songcard.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-songcard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './songcard.component.html',
   styleUrl: './songcard.component.css'
 })
 export class SongcardComponent implements OnInit{
+item :any;
+items : number[] = [1,2,3,4,5,6,7];
   ngOnInit(): void {
    
   }
