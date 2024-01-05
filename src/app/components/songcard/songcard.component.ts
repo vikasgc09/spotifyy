26ed3ad784d4fba46eb0b226db9690b31fd9fcb2
vikas Chauhan <vikasgc09@gmail.com>
2024-01-05 14:37:16 +0530

    INITIAL COMMIT

--- src/app/components/songcard/songcard.component.ts
+++ src/app/components/songcard/songcard.component.ts
@@ -1,16 +1,17 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-songcard',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor,FlexLayoutModule],
   templateUrl: './songcard.component.html',
   styleUrl: './songcard.component.css'
 })
 export class SongcardComponent implements OnInit{
 item :any;
-items : number[] = [1,2,3,4,5,6,7];
+items : number[] = [1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7, 1,2,3,4,5,6,7, 1,2,3,4,5,6,7];
   ngOnInit(): void {
    
   }
